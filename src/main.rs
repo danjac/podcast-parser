16c6557e6f3fdfa80d01b5aef1a3ee8e203d4eb4
Dan Jacob <danjac2018@gmail.com>
2024-01-24 21:48:28 +0200

    Handle request for async

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,31 @@ use rss::Channel;
 
 use tokio::task::JoinSet;
 
-async fn fetch_podcast(url: &str) -> Channel {
-    println!("running url: {}", url);
-    let response = reqwest::get(url).await.expect("could not fetch").bytes().await.expect("ok");
-    println!("done: {}", url);
-    Channel::read_from(&response[..]).expect("cannot parse XML")
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
 
     let mut set = JoinSet::new();
 
     let urls = vec![
-        "https://pod.link/1171270672.rss",
+        "https://pod.ink/1171270672.rss",
         "http://feeds.feedburner.com/thememorypalace",
         "https://feeds.acast.com/public/shows/cfd8aed5-6d63-4b2f-b325-46e4b5665583",
     ];
 
     for url in urls {
         set.spawn(async move { 
-            fetch_podcast(url).await
+            reqwest::get(url).await?.bytes().await
         });
     }
 
     while let Some(result) = set.join_next().await {
-        let channel = result?;
-        println!("Title: {:?}", channel.title);
-        //println!("Description: {:?}", channel.description);
-        //println!("Image: {:?}", channel.image);
-        println!("Episodes: {}", channel.items.len());
+        let response = result??;
+        let channel = Channel::read_from(&response[..])?;
+
+            println!("Title: {:?}", channel.title);
+            //println!("Description: {:?}", channel.description);
+            //println!("Image: {:?}", channel.image);
+            println!("Episodes: {}", channel.items.len());
 
     //     println!("Title: {:?}", channel.title);
     //     println!("Description: {:?}", channel.description);
