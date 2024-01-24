aa18724cd148dd38bc8ae1cc5f349809ac6256c4
Dan Jacob <danjac2018@gmail.com>
2024-01-24 21:12:32 +0200

    Using expect()

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,55 @@
 use std::error::Error;
 use rss::Channel;
 
-async fn fetch_podcast(url: &str) -> Result<(), Box<dyn Error>> {
-    let response = reqwest::get(url).await?.bytes().await?;
 
-    let channel = Channel::read_from(&response[..])?;
-    println!("Title: {:?}", channel.title);
-    println!("Description: {:?}", channel.description);
-    println!("Image: {:?}", channel.image);
-    println!("Episodes: {}", channel.items.len());
+use tokio::task::JoinSet;
 
-    if let Some(item) = channel.items.first() {
-        println!("LATEST EPISODE");
-        println!("Title: {:?}", item.title);
-        println!("GUID: {:?}", item.guid);
-        println!("Description: {:?}", item.description);
-        println!("Pub date: {:?}", item.pub_date);
-        println!("Itunes: {:?}", item.itunes_ext);
-        println!("Enclosure: {:?}", item.enclosure);
-    }
-
-    Ok(())
-
+async fn fetch_podcast(url: &str) -> Channel {
+    println!("running url: {}", url);
+    let response = reqwest::get(url).await.expect("could not fetch").bytes().await.expect("ok");
+    println!("done: {}", url);
+    Channel::read_from(&response[..]).expect("cannot parse XML")
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    fetch_podcast("https://pod.link/1171270672.rss").await
+
+    let mut set = JoinSet::new();
+
+    let urls = vec![
+        "https://pod.link/1171270672.rss",
+        "http://feeds.feedburner.com/thememorypalace",
+        "https://feeds.acast.com/public/shows/cfd8aed5-6d63-4b2f-b325-46e4b5665583",
+    ];
+
+    for url in urls {
+        set.spawn(async move { 
+            fetch_podcast(url).await
+        });
+    }
+
+    while let Some(result) = set.join_next().await {
+        let channel = result?;
+        println!("Title: {:?}", channel.title);
+        //println!("Description: {:?}", channel.description);
+        //println!("Image: {:?}", channel.image);
+        println!("Episodes: {}", channel.items.len());
+
+    //     println!("Title: {:?}", channel.title);
+    //     println!("Description: {:?}", channel.description);
+    //     println!("Image: {:?}", channel.image);
+    //     println!("Episodes: {}", channel.items.len());
+
+    //     if let Some(item) = channel.items.first() {
+    //         println!("LATEST EPISODE");
+    //         println!("Title: {:?}", item.title);
+    //         println!("GUID: {:?}", item.guid);
+    //         println!("Description: {:?}", item.description);
+    //         println!("Pub date: {:?}", item.pub_date);
+    //         println!("Itunes: {:?}", item.itunes_ext);
+    //         println!("Enclosure: {:?}", item.enclosure);
+    //     }
+     }
+
+    Ok(())
 }
